2227b291c36e663287ef5f2c526564d4a13e453c
Kriegslustig <github@ls7.ch>
2015-07-17 21:06:40 +0200

    Wait for all subscriptions

--- client/loader.js
+++ client/loader.js
@@ -2,7 +2,7 @@ mainContainer = document.getElementsByTagName('main')[0]
 header = document.getElementsByTagName('header')[0]
 
 Tracker.autorun(function () {
-  if(FlowRouter.subsReady('allPosts')) {
+  if(FlowRouter.subsReady()) {
     console.log('Hi')
     mainContainer.removeChild(
       mainContainer.children[0]
