94509560603dc90a9e40f47685bebcae7d47c719
Kriegslustig <github@ls7.ch>
2015-07-31 21:15:09 +0200

    Remove the delay on main.js

--- server/loader.js
+++ server/loader.js
@@ -3,12 +3,3 @@ altboiler.config({
   content: Assets.getText('loader/main.html'),
   css: Assets.getText('loader/styles.css')
 })
-
-// This is a little overkill
-// // Add an artificial delay
-// WebApp.rawConnectHandlers.stack = [{
-//   route: '/altboiler/main.js',
-//   handle: function (req, res, next) {
-//     return setTimeout(next, 4000)
-//   }
-// }].concat(WebApp.rawConnectHandlers.stack)
